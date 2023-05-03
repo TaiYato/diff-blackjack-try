45ec80a91cfd76f7bb37c491f46f9639f32b4c73
803745ctc <803745ctc@MA-CTC-INST-S24>
2023-05-03 13:02:01 -0400

    yessir

--- diff blackjack try/Program.cs
+++ diff blackjack try/Program.cs
@@ -5,9 +5,49 @@ class Blackjack
 {
     static void Main(string[] args)
     {
-        uint money = 1000;
         ConsoleColor defaultcolor = ConsoleColor.Green;
-        Console.ForegroundColor = defaultcolor;
+
+        Console.ForegroundColor = ConsoleColor.White;
+        Console.WriteLine("Welcome to Blackjack!");
+        Console.WriteLine();
+        
+        Console.Write("Do you know how to play blackjack? (yes or no): ");
+        string decision = Console.ReadLine().ToLower();
+        if (decision == "no")
+        
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("Blackjack is a card game where the goal is to get a hand value of 21 or as close to 21 as possible without going over.");
+            Console.WriteLine();
+            Console.WriteLine("Face cards (jack, queen, king) are worth 10 points, and aces can be worth either 1 or 11 points. An ace is worth 1 point after you recieve more than 2 cards");
+            Console.WriteLine();
+            Console.WriteLine("The dealer will deal two cards to each player, and then each player can choose to 'hit' (draw another card) or 'stand' (keep their current hand) or 'double down' (Doubling your bet and draw ONLY one more card.");
+            Console.WriteLine("If you double down, It will stand you after pulling one more card. It doubles your bet so make sure you have enough money to double down.");
+            Console.WriteLine();
+            Console.WriteLine("If a player's or the dealers hand goes over 21, they 'bust' if it's the dealer, you win. if it is you, you lose the game. If the player's hand is closer to 21 than the dealer's, you win.");
+            Console.WriteLine("If you and the dealer recieve the same number of points a 'push' (Tie) will happen. It returns the money you placed on your bet and restarts the game.");
+            Console.WriteLine();
+            Console.WriteLine("Hope that explained enough!! Let's get started now!!!");
+            Console.WriteLine();
+            Console.ForegroundColor = defaultcolor;
+        }
+        else if (decision == "yes")
+        {
+            Console.WriteLine("Great, let's get started!");
+            Console.WriteLine();
+        }
+        else
+        {
+            Console.WriteLine("Invalid choice. Please enter 'yes' or 'no'.");
+            Console.WriteLine();
+            Console.Clear();
+            Main(args); // restart the program
+            return;
+            Console.Clear();
+        }
+        
+       
+        uint money = 1000;       
         Console.WriteLine("Welcome to Blackjack! You have $" + money);
 
         //Betting
@@ -17,6 +57,7 @@ class Blackjack
             Console.Write("Enter your bet: ");
 
             uint bet = 0;
+     
             while (uint.TryParse(Console.ReadLine(), out bet) == false)
             {
                 Console.Write("You accidentally put a letter, invalid number, negative number, or nothing, please enter your bet again: ");
@@ -36,7 +77,7 @@ class Blackjack
     
            
             Console.WriteLine("---------------");
-            Console.Clear();           
+            Console.Clear();          
             Console.WriteLine("Welcome to Blackjack! You have $" + money);
             Console.WriteLine($"Current bet: " + bet);
 
@@ -153,7 +194,9 @@ class Blackjack
                     }
                     if (bet * 2 > money)
                     {
+                        Console.ForegroundColor = ConsoleColor.Red;
                         Console.WriteLine("You don't have enough money to double down!");
+                        Console.ForegroundColor = defaultcolor;
                         continue;
                     }
                     playerHand.Add(DealCard(deck));
